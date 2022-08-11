2a66c107de3985cb13e3e33f700cf0129265e9ce
rsteube <rsteube@users.noreply.github.com>
2022-08-12 00:05:43 +0200

    nushell style

--- internal/shell/nushell/action.go
+++ internal/shell/nushell/action.go
@@ -7,11 +7,13 @@ import (
 	"strings"
 
 	"github.com/rsteube/carapace/internal/common"
+	"github.com/rsteube/carapace/third_party/github.com/elves/elvish/pkg/ui"
 )
 
 type record struct {
-	Value       string `json:"value"`
-	Description string `json:"description,omitempty"`
+	Value        string `json:"value"`
+	Description  string `json:"description,omitempty"`
+	nushellStyle `json:",inline"`
 }
 
 var sanitizer = strings.NewReplacer(
@@ -43,8 +45,88 @@ func ActionRawValues(currentWord string, nospace bool, values common.RawValues) 
 			val.Value = val.Value + " "
 		}
 
-		vals[index] = record{Value: val.Value, Description: val.TrimmedDescription()}
+		vals[index] = record{Value: val.Value, Description: val.TrimmedDescription(), nushellStyle: toNushellstyle(val.Style)}
 	}
 	m, _ := json.Marshal(vals)
 	return string(m)
+}
+
+type nushellStyle struct {
+	Foreground string   `json:"foreground,omitempty"`
+	Background string   `json:"background,omitempty"`
+	Attributes []string `json:"attributes,omitempty"`
+}
+
+func toNushellstyle(_style string) nushellStyle {
+	s := parseStyle(_style)
+
+	attributes := make([]string, 0)
+	if s.Blink {
+		attributes = append(attributes, "slow_blink")
+	}
+	if s.Bold {
+		attributes = append(attributes, "bold")
+	}
+	if s.Dim {
+		attributes = append(attributes, "dim")
+	}
+	if s.Italic {
+		attributes = append(attributes, "italic")
+	}
+	if s.Inverse {
+		attributes = append(attributes, "negative")
+	}
+	if s.Underlined {
+		attributes = append(attributes, "underlined")
+	}
+
+	replacer := strings.NewReplacer(
+		"bright-black", "dark_grey",
+		"black", "black",
+		"bright-red", "red",
+		"red", "dark_red",
+		"bright-green", "green",
+		"green", "dark_green",
+		"bright-yellow", "yellow",
+		"yellow", "dark_yellow",
+		"bright-blue", "blue",
+		"blue", "dark_blue",
+		"bright-magenta", "magenta",
+		"magenta", "dark_magenta",
+		"bright-cyan", "cyan",
+		"cyan", "dark_cyan",
+		"bright-white", "white",
+		"white", "grey",
+	)
+
+	// TODO colorx -> ansi_(x)
+	// TODO #rrggbb -> rgb_(r,g,b)
+
+	fg := ""
+	if s.Foreground != nil {
+		fg = replacer.Replace(s.Foreground.String())
+	}
+
+	bg := ""
+	if s.Background != nil {
+		bg = replacer.Replace(s.Background.String())
+	}
+
+	// TODO patch color names
+	return nushellStyle{
+		Foreground: fg,
+		Background: bg,
+		Attributes: attributes,
+	}
+}
+
+// TODO copied from style package
+func parseStyle(s string) ui.Style {
+	stylings := make([]ui.Styling, 0)
+	for _, word := range strings.Split(s, " ") {
+		if styling := ui.ParseStyling(word); styling != nil {
+			stylings = append(stylings, styling)
+		}
+	}
+	return ui.ApplyStyling(ui.Style{}, stylings...)
 }
