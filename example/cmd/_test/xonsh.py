b076aad7889df07e4df41da0cd91a3b3cd621a97
Sourcery AI <>
2023-05-12 06:25:15 +0000

    'Refactored by Sourcery'

--- example/cmd/_test/xonsh.py
+++ example/cmd/_test/xonsh.py
@@ -3,23 +3,24 @@ from xonsh.completers.tools import *
 @contextual_command_completer
 def _example_completer(context):
     """carapace completer for example"""
-    if context.completing_command('example'):
-        from json import loads
-        from subprocess import Popen, PIPE
-        from xonsh.completers.tools import RichCompletion
-        
-        def fix_prefix(s):
-            """quick fix for partially quoted prefix completion ('prefix',<TAB>)"""
-            return s.translate(str.maketrans('', '', '\'"'))
+    if not context.completing_command('example'):
+        return
+    from json import loads
+    from subprocess import Popen, PIPE
+    from xonsh.completers.tools import RichCompletion
 
-        output, _ = Popen(['example', '_carapace', 'xonsh', *[a.value for a in context.args], fix_prefix(context.prefix)], stdout=PIPE, stderr=PIPE).communicate()
-        try:
-            result = {RichCompletion(c["Value"], display=c["Display"], description=c["Description"], prefix_len=len(context.raw_prefix), append_closing_quote=False) for c in loads(output)}
-        except:
-            result = {}
-        if len(result) == 0:
-            result = {RichCompletion(context.prefix, display=context.prefix, description='', prefix_len=len(context.raw_prefix), append_closing_quote=False)}
-        return result
+    def fix_prefix(s):
+        """quick fix for partially quoted prefix completion ('prefix',<TAB>)"""
+        return s.translate(str.maketrans('', '', '\'"'))
+
+    output, _ = Popen(['example', '_carapace', 'xonsh', *[a.value for a in context.args], fix_prefix(context.prefix)], stdout=PIPE, stderr=PIPE).communicate()
+    try:
+        result = {RichCompletion(c["Value"], display=c["Display"], description=c["Description"], prefix_len=len(context.raw_prefix), append_closing_quote=False) for c in loads(output)}
+    except:
+        result = {}
+    if not result:
+        result = {RichCompletion(context.prefix, display=context.prefix, description='', prefix_len=len(context.raw_prefix), append_closing_quote=False)}
+    return result
 
 
 from xonsh.completers._aliases import _add_one_completer
